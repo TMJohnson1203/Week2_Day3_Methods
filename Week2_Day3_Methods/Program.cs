9391b3e1d3440f43878b5ce5e82ff6dcbb383ce8
TMJohnson1203 <tmjohnson1203@gmail.com>
2017-10-16 15:07:24 -0400

    Initial commit

---
+++ Week2_Day3_Methods/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2_Day3_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int answer = Add(5, 7);
+            Console.WriteLine(answer);
+        }
+
+        public static int Add(int firstNum, int secondNum)
+            {
+            int sum = firstNum + secondNum;
+            return sum;
+            }
+
+
+
+    }
+}
